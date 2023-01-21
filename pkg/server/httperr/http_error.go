4222752617b6fc684c5f49f1c3ee3d2999a4bdb3
Henry Thomas <henryt1521@hotmail.com>
2023-01-21 23:40:17 +1100

    Added ResponseError to http_error.go

--- pkg/server/httperr/http_error.go
+++ pkg/server/httperr/http_error.go
@@ -2,27 +2,32 @@ package httperr
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/justjack1521/mevium/pkg/errors"
+	"github.com/justjack1521/mevium/pkg/genproto/common"
 	"net/http"
 )
 
+type ErrorResponse struct {
+	Error string `json:"error"`
+	Slug  string `json:"message"`
+}
+
+func ResponseError(header *common.ResponseHeader, ctx *gin.Context) {
+	httpRespondWithError(int(header.StatusCode), errors.NewSlugError(header.Error, header.Message), ctx)
+}
+
 func InternalError(err error, slug string, ctx *gin.Context) {
-	httpRespondWithError(err, slug, http.StatusInternalServerError, ctx)
+	httpRespondWithError(http.StatusInternalServerError, errors.NewSlugError(err.Error(), slug), ctx)
 }
 
 func UnauthorisedError(err error, slug string, ctx *gin.Context) {
-	httpRespondWithError(err, slug, http.StatusUnauthorized, ctx)
+	httpRespondWithError(http.StatusUnauthorized, errors.NewSlugError(err.Error(), slug), ctx)
 }
 
 func BadRequest(err error, slug string, ctx *gin.Context) {
-	httpRespondWithError(err, slug, http.StatusBadRequest, ctx)
+	httpRespondWithError(http.StatusBadRequest, errors.NewSlugError(err.Error(), slug), ctx)
 }
 
-type ErrorResponse struct {
-	Slug string `json:"message"`
-}
-
-func httpRespondWithError(err error, slug string, status int, ctx *gin.Context) {
-	ctx.AbortWithStatusJSON(status, ErrorResponse{
-		Slug: slug,
-	})
+func httpRespondWithError(status int, err error, ctx *gin.Context) {
+	ctx.AbortWithError(status, err)
 }
