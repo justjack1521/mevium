ad2df8d643e5eeb31a626db8e6629e60ad3f4b31
Henry Thomas <henryt1521@hotmail.com>
2023-01-31 14:20:28 +1100

    Added grpc server run function with options

--- pkg/server/grpc.go
+++ pkg/server/grpc.go
@@ -3,19 +3,23 @@ package server
 import (
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 	"net"
 )
 
-func RunGRPCServer(port string, credentials credentials.TransportCredentials, register func(server *grpc.Server)) {
+func RunGRPCServer(port string, register func(server *grpc.Server)) {
 	addr := fmt.Sprintf("0.0.0.0:%s", port)
-	RunGRPCServerOnAddr(addr, credentials, register)
+	RunGRPCServerOnAddr(addr, register)
 }
 
-func RunGRPCServerOnAddr(addr string, credentials credentials.TransportCredentials, registerServer func(server *grpc.Server)) {
+func RunGRPCServerWithOptions(port string, register func(server *grpc.Server), options ...grpc.ServerOption) {
+	addr := fmt.Sprintf("0.0.0.0:%s", port)
+	RunGRPCServerOnAddrWithOptions(addr, register, options...)
+}
 
-	svr := grpc.NewServer(grpc.Creds(credentials))
+func RunGRPCServerOnAddr(addr string, registerServer func(server *grpc.Server)) {
+
+	svr := grpc.NewServer()
 
 	registerServer(svr)
 
@@ -33,4 +37,24 @@ func RunGRPCServerOnAddr(addr string, credentials credentials.TransportCredentia
 		panic(err)
 	}
 
+}
+
+func RunGRPCServerOnAddrWithOptions(addr string, registerServer func(server *grpc.Server), options ...grpc.ServerOption) {
+	svr := grpc.NewServer(options...)
+
+	registerServer(svr)
+
+	reflection.Register(svr)
+
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Now listening on: %s", listen.Addr()))
+
+	err = svr.Serve(listen)
+	if err != nil {
+		panic(err)
+	}
 }
