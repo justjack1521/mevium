0a6e807feb80756508d71107bb23da6aae7af0dc
Henry Thomas <henryt1521@hotmail.com>
2023-02-08 22:43:03 +1100

    Added Game Data to data.proto, replacing game.proto

--- internal/scripts/protogen.bat
+++ internal/scripts/protogen.bat
@@ -6,7 +6,7 @@ protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_ou
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protoc --go_opt=paths=source_relative web.request.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protoc --go_opt=paths=source_relative web.response.proto
 
-protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/game --go_opt=paths=source_relative game.proto
+protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protog --go_opt=paths=source_relative data.proto
 
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go-grpc_out=github.com/justjack1521/mevium/pkg/genproto/services --go-grpc_opt=paths=source_relative --go-grpc_opt=require_unimplemented_servers=false access.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go-grpc_out=github.com/justjack1521/mevium/pkg/genproto/services --go-grpc_opt=paths=source_relative --go-grpc_opt=require_unimplemented_servers=false web.service.proto
