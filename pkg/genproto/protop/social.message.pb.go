12494113a4bf48c820538a348832093a4e0179f8
Henry Thomas <henryt1521@hotmail.com>
2023-02-26 23:00:51 +1100

    Added UpdateRentalCardRequest and UpdateRentalCardResponse

--- pkg/genproto/protop/social.message.pb.go
+++ pkg/genproto/protop/social.message.pb.go
@@ -368,6 +368,91 @@ func (*UpdatePlayerCommentResponse) Descriptor() ([]byte, []int) {
 	return file_social_message_proto_rawDescGZIP(), []int{7}
 }
 
+type UpdateRentalCardRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	RentalCard string `protobuf:"bytes,1,opt,name=rental_card,json=rentalCard,proto3" json:"rental_card,omitempty"`
+}
+
+func (x *UpdateRentalCardRequest) Reset() {
+	*x = UpdateRentalCardRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_social_message_proto_msgTypes[8]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateRentalCardRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateRentalCardRequest) ProtoMessage() {}
+
+func (x *UpdateRentalCardRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_social_message_proto_msgTypes[8]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateRentalCardRequest.ProtoReflect.Descriptor instead.
+func (*UpdateRentalCardRequest) Descriptor() ([]byte, []int) {
+	return file_social_message_proto_rawDescGZIP(), []int{8}
+}
+
+func (x *UpdateRentalCardRequest) GetRentalCard() string {
+	if x != nil {
+		return x.RentalCard
+	}
+	return ""
+}
+
+type UpdateRentalCardResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *UpdateRentalCardResponse) Reset() {
+	*x = UpdateRentalCardResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_social_message_proto_msgTypes[9]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateRentalCardResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateRentalCardResponse) ProtoMessage() {}
+
+func (x *UpdateRentalCardResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_social_message_proto_msgTypes[9]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateRentalCardResponse.ProtoReflect.Descriptor instead.
+func (*UpdateRentalCardResponse) Descriptor() ([]byte, []int) {
+	return file_social_message_proto_rawDescGZIP(), []int{9}
+}
+
 var File_social_message_proto protoreflect.FileDescriptor
 
 var file_social_message_proto_rawDesc = []byte{
@@ -398,11 +483,16 @@ var file_social_message_proto_rawDesc = []byte{
 	0x18, 0x0a, 0x07, 0x63, 0x6f, 0x6d, 0x6d, 0x65, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09,
 	0x52, 0x07, 0x63, 0x6f, 0x6d, 0x6d, 0x65, 0x6e, 0x74, 0x22, 0x1d, 0x0a, 0x1b, 0x55, 0x70, 0x64,
 	0x61, 0x74, 0x65, 0x50, 0x6c, 0x61, 0x79, 0x65, 0x72, 0x43, 0x6f, 0x6d, 0x6d, 0x65, 0x6e, 0x74,
-	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6a, 0x75, 0x73, 0x74, 0x6a, 0x61, 0x63, 0x6b, 0x31,
-	0x35, 0x32, 0x31, 0x2f, 0x6d, 0x65, 0x76, 0x69, 0x75, 0x6d, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x67,
-	0x65, 0x6e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x70, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x3a, 0x0a, 0x17, 0x55, 0x70, 0x64, 0x61,
+	0x74, 0x65, 0x52, 0x65, 0x6e, 0x74, 0x61, 0x6c, 0x43, 0x61, 0x72, 0x64, 0x52, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x12, 0x1f, 0x0a, 0x0b, 0x72, 0x65, 0x6e, 0x74, 0x61, 0x6c, 0x5f, 0x63, 0x61,
+	0x72, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a, 0x72, 0x65, 0x6e, 0x74, 0x61, 0x6c,
+	0x43, 0x61, 0x72, 0x64, 0x22, 0x1a, 0x0a, 0x18, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x52, 0x65,
+	0x6e, 0x74, 0x61, 0x6c, 0x43, 0x61, 0x72, 0x64, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
+	0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6a,
+	0x75, 0x73, 0x74, 0x6a, 0x61, 0x63, 0x6b, 0x31, 0x35, 0x32, 0x31, 0x2f, 0x6d, 0x65, 0x76, 0x69,
+	0x75, 0x6d, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x67, 0x65, 0x6e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x70, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -417,7 +507,7 @@ func file_social_message_proto_rawDescGZIP() []byte {
 	return file_social_message_proto_rawDescData
 }
 
-var file_social_message_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
+var file_social_message_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
 var file_social_message_proto_goTypes = []interface{}{
 	(*UpdatePlayerPresenceRequest)(nil),   // 0: presence.UpdatePlayerPresenceRequest
 	(*UpdatePlayerPresenceResponse)(nil),  // 1: presence.UpdatePlayerPresenceResponse
@@ -427,6 +517,8 @@ var file_social_message_proto_goTypes = []interface{}{
 	(*UpdatePlayerCompanionResponse)(nil), // 5: presence.UpdatePlayerCompanionResponse
 	(*UpdatePlayerCommentRequest)(nil),    // 6: presence.UpdatePlayerCommentRequest
 	(*UpdatePlayerCommentResponse)(nil),   // 7: presence.UpdatePlayerCommentResponse
+	(*UpdateRentalCardRequest)(nil),       // 8: presence.UpdateRentalCardRequest
+	(*UpdateRentalCardResponse)(nil),      // 9: presence.UpdateRentalCardResponse
 }
 var file_social_message_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -538,6 +630,30 @@ func file_social_message_proto_init() {
 				return nil
 			}
 		}
+		file_social_message_proto_msgTypes[8].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateRentalCardRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_social_message_proto_msgTypes[9].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateRentalCardResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -545,7 +661,7 @@ func file_social_message_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_social_message_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   8,
+			NumMessages:   10,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
