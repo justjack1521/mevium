403856c401bc2df14e3f1f5d0fb196da13074a46
Henry Thomas <henryt1521@hotmail.com>
2023-02-28 21:39:26 +1100

    Added ClientDisconnected message

--- pkg/genproto/protom/notification.message.trx.go
+++ pkg/genproto/protom/notification.message.trx.go
@@ -13,3 +13,15 @@ func NewClientConnectedMessage(bytes []byte) (*ClientConnected, error) {
 func (x *ClientConnected) MarshallBinary() ([]byte, error) {
 	return proto.Marshal(x)
 }
+
+func NewClientDisconnectedMessage(bytes []byte) (*ClientDisconnected, error) {
+	req := &ClientDisconnected{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *ClientDisconnected) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
