c549eb728f2bc88c4dce596eee80f8c4e3935b7b
Henry Thomas <henryt1521@hotmail.com>
2023-03-04 23:06:27 +1100

    Added message.game.proto

--- internal/scripts/protogen.bat
+++ internal/scripts/protogen.bat
@@ -10,6 +10,7 @@ protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_ou
 
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protom --go_opt=paths=source_relative message.notification.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protom --go_opt=paths=source_relative message.internal.proto
+protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protom --go_opt=paths=source_relative message.game.proto
 
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protog --go_opt=paths=source_relative data.proto
 
--- pkg/genproto/protom/message.notification.trx.go
+++ pkg/genproto/protom/message.notification.trx.go
@@ -10,6 +10,10 @@ func NewNotification(bytes []byte) (*Notification, error) {
 	return req, nil
 }
 
+func (x *Notification) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
 func NewSocialDataNotification(bytes []byte) (*SocialDataNotification, error) {
 	req := &SocialDataNotification{}
 	if err := proto.Unmarshal(bytes, req); err != nil {
@@ -18,10 +22,42 @@ func NewSocialDataNotification(bytes []byte) (*SocialDataNotification, error) {
 	return req, nil
 }
 
-func (x *Notification) MarshallBinary() ([]byte, error) {
+func (x *SocialDataNotification) MarshallBinary() ([]byte, error) {
 	return proto.Marshal(x)
 }
 
-func (x *SocialDataNotification) MarshallBinary() ([]byte, error) {
+func NewBattleComplete(bytes []byte) (*BattleComplete, error) {
+	req := &BattleComplete{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *BattleComplete) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewBattleRankingInfo(bytes []byte) (*BattleRankingInfo, error) {
+	req := &BattleRankingInfo{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *BattleRankingInfo) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewBattleRankingClaim(bytes []byte) (*BattleRankingClaim, error) {
+	req := &BattleRankingClaim{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *BattleRankingClaim) MarshallBinary() ([]byte, error) {
 	return proto.Marshal(x)
 }
