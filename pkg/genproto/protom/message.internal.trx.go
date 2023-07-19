76504072a89ffac4cb0e677b00caf2c97a89031c
Henry Thomas <henryt1521@hotmail.com>
2023-07-19 21:57:51 +1000

    Deleted social.message.trx.go

--- pkg/genproto/protom/message.internal.trx.go
+++ pkg/genproto/protom/message.internal.trx.go
@@ -49,3 +49,63 @@ func NewPlayerProfileCreatedMessage(bytes []byte) (*PlayerProfileCreatedMessage,
 func (x *PlayerProfileCreatedMessage) MarshallBinary() ([]byte, error) {
 	return proto.Marshal(x)
 }
+
+func NewPlayerPresenceUpdateMessage(bytes []byte) (*PlayerPresenceUpdateMessage, error) {
+	req := &PlayerPresenceUpdateMessage{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *PlayerPresenceUpdateMessage) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewPlayerPositionUpdateMessage(bytes []byte) (*PlayerPositionUpdateMessage, error) {
+	req := &PlayerPositionUpdateMessage{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *PlayerPositionUpdateMessage) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewPlayerCompanionUpdateMessage(bytes []byte) (*PlayerCompanionUpdateMessage, error) {
+	req := &PlayerCompanionUpdateMessage{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *PlayerCompanionUpdateMessage) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewPlayerCommentUpdateMessage(bytes []byte) (*PlayerCommentUpdateMessage, error) {
+	req := &PlayerCommentUpdateMessage{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *PlayerCommentUpdateMessage) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewPlayerRentalCardUpdateMessage(bytes []byte) (*PlayerRentalCardUpdateMessage, error) {
+	req := &PlayerRentalCardUpdateMessage{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *PlayerRentalCardUpdateMessage) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
