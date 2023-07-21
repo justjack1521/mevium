00068fa177087178a9742043863003a1ad5e515d
Henry Thomas <henryt1521@hotmail.com>
2023-07-21 22:25:12 +1000

    Added social.proto

--- internal/scripts/protogen.bat
+++ internal/scripts/protogen.bat
@@ -10,6 +10,7 @@ protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_ou
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protor --go_opt=paths=source_relative rank.request.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protor --go_opt=paths=source_relative rank.response.proto
 
+protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protop --go_opt=paths=source_relative social.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protop --go_opt=paths=source_relative social.request.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protop --go_opt=paths=source_relative social.response.proto
 
