e7ea62b072a7b8e23189f4670f545b32bd03d657
justjack1521 <henryt1521@hotmail.com>
2023-09-03 21:16:06 +1000

    Added ProtoSocialChallengeInstance and ProtoPlayerSocialChallengeEntry

--- internal/scripts/protogen.bat
+++ internal/scripts/protogen.bat
@@ -19,6 +19,7 @@ protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_ou
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protom --go_opt=paths=source_relative message.internal.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protom --go_opt=paths=source_relative message.game.proto
 
+protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protosc --go_opt=paths=source_relative challenge.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protosc --go_opt=paths=source_relative challenge.request.proto
 protoc --proto_path=github.com/justjack1521/mevium/internal/api/protobuf --go_out=github.com/justjack1521/mevium/pkg/genproto/protosc --go_opt=paths=source_relative challenge.response.proto
 
