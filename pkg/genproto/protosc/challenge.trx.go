97e74666f4ed62adeb4b5ff86449e73f490f8b6b
justjack1521 <henryt1521@hotmail.com>
2023-09-06 13:16:13 +1000

    Added GetPlayerChallenge to MeviusRankService

--- pkg/genproto/protosc/challenge.trx.go
+++ pkg/genproto/protosc/challenge.trx.go
@@ -2,7 +2,7 @@ package protosc
 
 import "google.golang.org/protobuf/proto"
 
-func NewFetchPlayerRankingInfoRequest(bytes []byte) (*JoinSocialChallengeRequest, error) {
+func NewJoinSocialChallengeRequest(bytes []byte) (*JoinSocialChallengeRequest, error) {
 	req := &JoinSocialChallengeRequest{}
 	if err := proto.Unmarshal(bytes, req); err != nil {
 		return nil, err
@@ -11,5 +11,17 @@ func NewFetchPlayerRankingInfoRequest(bytes []byte) (*JoinSocialChallengeRequest
 }
 
 func (x *JoinSocialChallengeResponse) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewGetPlayerChallengeRequest(bytes []byte) (*GetPlayerChallengeRequest, error) {
+	req := &GetPlayerChallengeRequest{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *GetPlayerChallengeRequest) MarshallBinary() ([]byte, error) {
 	return proto.Marshal(x)
 }
