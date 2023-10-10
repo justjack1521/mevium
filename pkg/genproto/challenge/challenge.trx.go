160b57a3e3046548d41f67efbb214ba0eab11b8e
justjack1521 <henryt1521@hotmail.com>
2023-10-10 22:11:34 +1100

    Big change to file structure!

---
+++ pkg/genproto/challenge/challenge.trx.go
@@ -0,0 +1,27 @@
+package protochallenge
+
+import "google.golang.org/protobuf/proto"
+
+func NewJoinSocialChallengeRequest(bytes []byte) (*JoinSocialChallengeRequest, error) {
+	req := &JoinSocialChallengeRequest{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *JoinSocialChallengeResponse) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func NewGetPlayerChallengeRequest(bytes []byte) (*GetPlayerChallengeRequest, error) {
+	req := &GetPlayerChallengeRequest{}
+	if err := proto.Unmarshal(bytes, req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func (x *GetPlayerChallengeResponse) MarshallBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
