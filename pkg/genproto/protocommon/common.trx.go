a7c86a4f5ac4358d22a67e1862ee862e9227b8ae
justjack1521 <henryt1521@hotmail.com>
2024-10-08 22:46:56 +1100

    added fields

--- pkg/genproto/protocommon/common.trx.go
+++ pkg/genproto/protocommon/common.trx.go
@@ -1,13 +1,8 @@
 package protocommon
 
 import (
-	"fmt"
 	"google.golang.org/protobuf/proto"
 )
-
-func (x *Response) Error() string {
-	return fmt.Sprintf("status %d: err %v", x.Header.ErrorCode, x.Header.Error)
-}
 
 func (x *Response) MarshallBinary() ([]byte, error) {
 	bytes, err := proto.Marshal(x)
